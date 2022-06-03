4a8ed7a66d78cfb769b8598b7a8f84ba4fc6510e
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-06-03 11:49:35 -0300

    import transactions mod

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod product;
-mod user;
+mod transactions;
+mod users;
 fn main() {
     let mut products: Vec<product::Product> = Vec::new();
 
@@ -10,11 +11,16 @@ fn main() {
 
     products[0].set_price(10.9);
 
-    let mut user_1 = user::User::new(user::Types::USER, String::from("Francisco"), 1000.00);
+    let mut user_1 = users::User::new(users::Types::USER, String::from("Francisco"), 1000.00);
 
     user_1.add_product_in_cart(&products[0], 20);
     user_1.add_product_in_cart(&products[0], 30);
     user_1.user_info();
     user_1.add_product_in_cart(&products[0], 30);
+    user_1.user_info();
+
+    //user_1.pay();
+
+    transactions::pay(&mut user_1);
     user_1.user_info();
 }
