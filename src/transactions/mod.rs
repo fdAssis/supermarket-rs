bca8b6e620dcb4946d81dcfe69ad864a08d80011
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-06-03 17:24:23 -0300

    create mod transactions

--- src/transactions/mod.rs
+++ src/transactions/mod.rs
@@ -1,15 +1,1 @@
-use crate::users::User;
-
-pub fn pay(user: &mut User) {
-  let mut total: f32 = 0.0;
-
-  for i in 0..user.get_cart().len() {
-    total += user.get_cart()[i].get_price() * (user.get_cart()[i].get_quantity() as f32);
-  }
-
-  assert!(!(user.get_balance() < total), "❌ Saldo Insuficiente");
-
-  user.set_balance(user.get_balance() - total);
-
-  println!("✅ Compra realizada com sucesso");
-}
+pub mod user_transactions;
